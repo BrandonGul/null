cc6417d64ec995c2385fa992bb02f132ca67e854
BrandonGul <bsguly@gmail.com>
2025-02-07 23:15:43 -0600

    Removed text animation Added in image loading skeleton

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,9 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://null.contact',
-
+  experimental: {
+    responsiveImages: true,
+  },
   vite: {
     plugins: [tailwindcss()]
   }
